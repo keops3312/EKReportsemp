cf3be7554effb41db37be27586e4ffa8f287dd88
keops3312 <keops_3312@hotmail.com>
2018-12-19 13:27:42 -0600

    creating file
    creating

--- EKReportsemp.Common/Models/CRM_SEMP_SALDOS.cs
+++ EKReportsemp.Common/Models/CRM_SEMP_SALDOS.cs
@@ -4,13 +4,10 @@ namespace EKReportsemp.Common.Models
 {
 
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
     public class CRM_SEMP_SALDOS
     {
-
+        [Key]
         public int idCRM_SEMP_SALDOS { get; set; }
         public string caja { get; set; }
         public string numcaja { get; set; }
